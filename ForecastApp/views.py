a2b979c682eb0f065e23b4590900b0c5ae3cf544
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2023-01-04 21:09:57 +0530

    Added old data in graph

--- ForecastApp/views.py
+++ ForecastApp/views.py
@@ -23,16 +23,29 @@ def get_preds(request):
     dataset = pd.read_csv("./static/data/predictions.csv")
     dataset["Dates"] = pd.to_datetime(dataset["Dates"], format="%d-%m-%Y")
     # print(dataset)
+    old_dataset = pd.read_csv(f"./static/data/datasets/PLID_{product_id}.csv")
+    print(old_dataset.columns)
+    old_dataset['Booking_Date'] = pd.to_datetime(old_dataset["Booking_Date"], format="%d-%m-%Y")
+
     date = pd.to_datetime(str(date), format="%Y-%m-%d")
     print(date)
+
     predictions = dataset[dataset["PLID"]==product_id]
     predictions = predictions[predictions["Dates"].between(left=predictions.Dates.min(), right=date)]
     
     predictions.sort_values("Dates", inplace=True)
+    old_dataset.sort_values("Booking_Date", inplace=True)
+
+    print("Reached")
+
     predictions.Dates = predictions.Dates.astype("str")
+    old_dataset.Booking_Date = old_dataset.Booking_Date.astype("str")
+
     data = {
-        "dates":list(predictions.Dates.values),
-        "preds":list(predictions.predictions_final.values)
+        "dates":list(old_dataset.Booking_Date.values)+list(predictions.Dates.values),
+        "preds":list(predictions.predictions_final.values),
+        "old_preds":list(old_dataset.Booked_Qty.values)
     }
+    print(data["dates"])
     return Response(data)
 
