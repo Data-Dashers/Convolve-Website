9ffbf62bb5a9cb31da7b098fe8ea5ceb41621252
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2023-01-05 02:55:20 +0530

    Added Size data to tooltip

--- ForecastApp/views.py
+++ ForecastApp/views.py
@@ -16,19 +16,22 @@ def home(request):
 
 @api_view(['POST'])
 def get_preds(request):
-    print(request.data)
+    # print(request.data)
     product_id = request.data.get('product_id')
     date = request.data.get('date')
-    print(product_id, date)
-    dataset = pd.read_csv("./static/data/predictions.csv")
+    # print(product_id, date)
+    dataset = pd.read_csv("./static/data/predictions_1.csv")
     dataset["Dates"] = pd.to_datetime(dataset["Dates"], format="%d-%m-%Y")
     # print(dataset)
     old_dataset = pd.read_csv(f"./static/data/datasets/PLID_{product_id}.csv")
-    print(old_dataset.columns)
+    # print(old_dataset.columns)
     old_dataset['Booking_Date'] = pd.to_datetime(old_dataset["Booking_Date"], format="%d-%m-%Y")
 
+    train_data = pd.read_csv(f"./static/data/train_with_size.csv")
+    train_data = train_data[train_data.PLID==product_id]
+
     date = pd.to_datetime(str(date), format="%Y-%m-%d")
-    print(date)
+    # print(date)
 
     predictions = dataset[dataset["PLID"]==product_id]
     predictions = predictions[predictions["Dates"].between(left=predictions.Dates.min(), right=date)]
@@ -36,16 +39,26 @@ def get_preds(request):
     predictions.sort_values("Dates", inplace=True)
     old_dataset.sort_values("Booking_Date", inplace=True)
 
-    print("Reached")
+
+
+    # print("Reached")
 
     predictions.Dates = predictions.Dates.astype("str")
     old_dataset.Booking_Date = old_dataset.Booking_Date.astype("str")
 
+    sizes = dict()
+
+    for i in old_dataset.Booking_Date.values:
+      # print(train_data[train_data.Booking_Date==i]["Size"].values[0])
+      sizes[f"{i}"] = train_data[train_data.Booking_Date==i]["Size"].values[0]
+    for i in predictions.Dates.values:
+      sizes[f"{i}"] = predictions[predictions.Dates==i]["test_size"].values[0]
     data = {
         "dates":list(old_dataset.Booking_Date.values)+list(predictions.Dates.values),
-        "preds":list(predictions.predictions_final.values),
-        "old_preds":list(old_dataset.Booked_Qty.values)
+        "preds":[0 for i in list(old_dataset.Booked_Qty.values)]+list(predictions.predictions_final.values),
+        "old_preds":list(old_dataset.Booked_Qty.values),
+        "sizes":sizes
+        
     }
-    print(data["dates"])
     return Response(data)
 
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Convolve-Website
+
+## Project Description:
+
+- This is python project to forecast sales of different products.
+- Technologies used - Django, ChartJS, Django-Rest_Framework
+- This project was done for the second round of Convolve Competition organized by Cisco.
+
+
+## Installation and Testing:
+
+- Clone the repository using
+
+    ```
+    git clone https://github.com/Data-Dashers/Convolve-Website.git
+    ```
+- Install Python
+- Install virtualenv packages using 
+
+    ```
+    pip install virtualenv
+    ```
+- Create a virtual environment with
+
+    ```
+    virtualenv <env_name>
+    ```
+- Activate the virtual environment using :
+
+    For Windows
+
+    ```
+    .env_name/Scripts/activate
+    ``` 
+    For Linux
+    
+    ```
+    source env_name/bin/activate
+    ```
+- Change directory to the project folder
+- Install the required packages using 
+    
+    ```
+    pip install -r requirements.txt
+    ```
+- Migrate the changes inside the project
+
+    ```
+    python manage.py migrate
+    ```
+- Run the server
+    ```
+    python manage.py runserver
+    ```
+- Open localhost:8000 in browser to use the UI
