83dee3fabbc876c91eef5da19a9a12c9f8d74d99
Jagteshver Singh Sahni <85994817+JS-CodeGamer@users.noreply.github.com>
2023-01-05 07:18:30 +0530

    fix:fixxed graph line sequence

--- ForecastApp/views.py
+++ ForecastApp/views.py
@@ -7,32 +7,36 @@ from django.shortcuts import render
 import os
 import pandas as pd
 
+
 def home(request):
-  context = {
-    "productIDs" : [id[5:-4] for id in os.listdir('./static/data/datasets/')]
-  }
+    context = {"productIDs": [id[5:-4] for id in os.listdir("./static/data/datasets/")]}
 
-  return render(request, 'base.html', {"context":context})
+    return render(request, "base.html", {"context": context})
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def get_preds(request):
     print(request.data)
-    product_id = request.data.get('product_id')
-    date = request.data.get('date')
+    product_id = request.data.get("product_id")
+    date = request.data.get("date")
     print(product_id, date)
     dataset = pd.read_csv("./static/data/predictions.csv")
     dataset["Dates"] = pd.to_datetime(dataset["Dates"], format="%d-%m-%Y")
     # print(dataset)
     old_dataset = pd.read_csv(f"./static/data/datasets/PLID_{product_id}.csv")
     print(old_dataset.columns)
-    old_dataset['Booking_Date'] = pd.to_datetime(old_dataset["Booking_Date"], format="%d-%m-%Y")
+    old_dataset["Booking_Date"] = pd.to_datetime(
+        old_dataset["Booking_Date"], format="%d-%m-%Y"
+    )
 
     date = pd.to_datetime(str(date), format="%Y-%m-%d")
     print(date)
 
-    predictions = dataset[dataset["PLID"]==product_id]
-    predictions = predictions[predictions["Dates"].between(left=predictions.Dates.min(), right=date)]
-    
+    predictions = dataset[dataset["PLID"] == product_id]
+    predictions = predictions[
+        predictions["Dates"].between(left=predictions.Dates.min(), right=date)
+    ]
+
     predictions.sort_values("Dates", inplace=True)
     old_dataset.sort_values("Booking_Date", inplace=True)
 
@@ -40,12 +44,18 @@ def get_preds(request):
 
     predictions.Dates = predictions.Dates.astype("str")
     old_dataset.Booking_Date = old_dataset.Booking_Date.astype("str")
+    old_preds = list(old_dataset.Booked_Qty.values)
+    preds = [None for i in range(len(list(old_dataset.Booked_Qty.values)))] + list(
+        predictions.predictions_final.values
+    )
+    preds[len(old_preds) - 1] = old_preds[len(old_preds) - 1]
+    old_preds = [int(i) for i in old_preds]
+    preds = [int(i) if i else i for i in preds]
 
     data = {
-        "dates":list(old_dataset.Booking_Date.values)+list(predictions.Dates.values),
-        "preds":list(predictions.predictions_final.values),
-        "old_preds":list(old_dataset.Booked_Qty.values)
+        "dates": list(old_dataset.Booking_Date.values) + list(predictions.Dates.values),
+        "preds": preds,
+        "old_preds": old_preds,
     }
     print(data["dates"])
     return Response(data)
-
